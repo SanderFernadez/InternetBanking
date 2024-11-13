7145b46490e1106fe51299048129405d82f504d9
Sander Fernandez <sanderfernandez0812@gmail.com>
2024-11-13 04:02:21 -0400

    avance con transacciones

--- InternetBanking.Core.Application/Dtos/SystemDates/BankAccountResponse.cs
+++ InternetBanking.Core.Application/Dtos/SystemDates/BankAccountResponse.cs
@@ -1,18 +1,9 @@
 ﻿
-using InternetBanking.Core.Application.Dtos.Account;
-using InternetBanking.Core.Application.ViewModels.BankAccounts;
-using InternetBanking.Core.Application.ViewModels.Payments;
-using InternetBanking.Core.Application.ViewModels.Transactions;
 
 namespace InternetBanking.Core.Application.Dtos.SystemDates
 {
     public class BankAccountResponse
     {
-        //public ICollection<AuthenticationResponse>? TotalUsers { get; set; }
-        //public ICollection<BankAccountViewModel>? TotalAccounts { get; set; }
-        //public ICollection<PaymentViewModel>? TotalPayments { get; set; }
-        //public ICollection<TransactionViewModel>? TotalTransactions { get; set; } 
-        
         
         public int? TotalUsers { get; set; }
         public int? TotalAccounts { get; set; }
